a53e863f3ec618e3e26b3069da1e381392de9930
Dmitry Korchagin <touchzie@yandex.ru>
2018-04-27 03:19:43 +0300

    initial commit

---
+++ ubereats/gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require("gulp");
+const browserSync = require("browser-sync");
+const postcss = require("gulp-postcss");
+const cssnext = require("postcss-cssnext");
+const autoprefixer = require("autoprefixer");
+const minifycss = require("gulp-csso");
+const imagemin = require("gulp-imagemin");
+const cache = require("gulp-cache");
+
+gulp.task("css", function() {
+  return gulp
+    .src("src/styles/*.css")
+    .pipe(postcss([cssnext(), autoprefixer({ browsers: ["last 5 version"] })]))
+    .pipe(minifycss())
+    .pipe(gulp.dest("build/styles"));
+});
+
+gulp.task("img", function() {
+  return gulp
+    .src("src/images/**/*.*")
+    .pipe(cache(imagemin()))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("default", ["css", "img"], function() {
+  gulp.watch("src/styles/*.css", ["css"]);
+  gulp.watch("src/images/**/*.*", ["img"]);
+});
